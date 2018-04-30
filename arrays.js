e2fa3788a1b92b80d63bd7c664d6c7213b9f3137
Drew Rollins <drewrollins14@gmail.com>
2018-04-30 04:50:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,8 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
-  return chocolateBars.pop();
+  chocolateBars.pop();
+  return chocolateBars;
 }
 
 function removeElementFromEndOfArray(chocolateBars){
