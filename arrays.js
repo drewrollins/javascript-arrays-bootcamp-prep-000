3f521d03d5277c7dfaacd46c5cb14754463ae6e2
Drew Rollins <drewrollins14@gmail.com>
2018-04-30 05:01:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 var candy = 'foo'
-function addElementToBeginningOfArray(chocolateBars, candy) {
-  [chocolateBars, ...candy];
+function addElementToBeginningOfArray(chocolateBars, candy){
+  return [candy, ...chocolateBars];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
