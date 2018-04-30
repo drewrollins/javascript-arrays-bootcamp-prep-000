c7ab6e69e74911938d3900b97833ad55c8da2cbb
Drew Rollins <drewrollins14@gmail.com>
2018-04-30 03:39:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,42 +1,41 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element) {
-  return [...element, array]
+  return [...element, array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
-  return array
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray(array, element) {
-  return [...array, element]
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.unshift(element)
-  return array
+  array.unshift(element);
+  return array;
 }
 
 function accessElementInArray(array) {
-  return array[2]
+  return array[2];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array, element) {
-  return array.shift(0)
+  return array.shift(0);
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array = array.slice(0)
-  return array
+  array = array.slice(0);
+  return array;
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop(0, 1)
-  array.length
+  return array.pop(0, 1);
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(2)
-  array.length
+  array.slice(2);
+  array.length;
 }
 
